d49c1dc3a9e1420e8087dc86b00a12fb6cb001eb
Mohammed-Shehsin <115590581+Mohammed-Shehsin@users.noreply.github.com>
2023-03-25 01:47:02 +0100

    Sample Qiz app

---
+++ CheckList/__astcache/c@@users@moham@onedrive@desktop@projects@checklist/checklist_unit.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+
+#ifndef CheckList_unitH
+#define CheckList_unitH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.ListBox.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Label1;
+	TListBox *TextListBox;
+	TButton *AddButton;
+	TEdit *TextEdit;
+	void __fastcall AddButtonClick(TObject *Sender);
+	void __fastcall TextListBoxItemClick(TCustomListBox * const Sender, TListBoxItem * const Item);
+
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
---
+++ Quiz/Unit1.h
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *QuestionLabel;
+	TLabel *Label2;
+	TLabel *PointLabel;
+	TButton *ConfirmButton;
+	TRadioButton *Answer1RadioButton;
+	TRadioButton *Answer2RadioButton;
+	TRadioButton *Answer3RadioButton;
+	void __fastcall Answer1RadioButtonChange(TObject *Sender);
+	void __fastcall Answer2RadioButtonChange(TObject *Sender);
+	void __fastcall Answer3RadioButtonChange(TObject *Sender);
+	void __fastcall ConfirmButtonClick(TObject *Sender);
+	
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
